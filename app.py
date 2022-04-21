6bcd9ff6583cf9a38abb274263d8604973dc9176
davyhermanus <34769065+davyhermanus@users.noreply.github.com>
2022-04-21 11:40:19 +0700

    Create app.py

---
+++ app.py
@@ -0,0 +1,4 @@
+import streamlit as st
+st.title("Image Classification with Google's Teachable Machine")
+st.header("Arhictecture Image Classification")
+st.text("Upload an image of house for classification")
